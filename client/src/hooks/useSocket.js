63c0566ff9575564343c156e0f0cbeaf43668f93
Bhavik <bhavikpatel773241@gmail.com>
2026-03-12 17:12:45 -0400

    feat: add useSocket custom hook

---
+++ client/src/hooks/useSocket.js
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from "react";
+import { io } from "socket.io-client";
+
+export function useSocket(url) {
+  const socket = useRef(null);
+  useEffect(() => {
+    socket.current = io(url);
+    return () => socket.current?.disconnect();
+  }, [url]);
+  return socket;
+}
