ad99b09118173eb3670846e1b1b697babe722221
Bhavik <bhavikpatel773241@gmail.com>
2026-03-13 19:44:34 -0400

    feat: add socket event handlers for typing indicators

---
+++ server/socket/handlers.js
@@ -0,0 +1,9 @@
+// Socket event handlers
+export function registerHandlers(io, socket) {
+  socket.on("typing:start", ({ channelId, userId }) => {
+    socket.to(channelId).emit("user:typing", { userId });
+  });
+  socket.on("typing:stop", ({ channelId, userId }) => {
+    socket.to(channelId).emit("user:stopped_typing", { userId });
+  });
+}
